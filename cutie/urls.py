d9a5d14b172006db6ed0106b5f226656fef907eb
Justin Le <jle043@ucr.edu>
2019-07-16 20:00:33 -0700

    Basic email Auth working

--- cutie/urls.py
+++ cutie/urls.py
@@ -1,8 +1,14 @@
-
+from django.conf.urls import url
 from django.urls import path
+from django.urls import include
 
 from . import views
 
 urlpatterns = [
         path('', views.index, name='index'),
+        path('email/', views.email, name='email'),
+        path('success/', views.success, name='success'),
+        path('signup/', views.signup, name='signup'),
+        path('activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/',
+        views.activate, name='activate'),
 ]
--- cutie/views.py
+++ cutie/views.py
@@ -1,7 +1,86 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from  .forms import ContactForm
+from django.core.mail import send_mail, BadHeaderError
+from backend.settings import DEFAULT_FROM_EMAIL
+from .forms import SignupForm
+from django.contrib.auth.models import User
+from django.core.mail import EmailMessage
+from .tokens import account_activation_token
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from django.contrib.auth import login, authenticate
+from django.http import JsonResponse
+
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html', context={},)
+
+def email(request):
+    if request.method == 'GET':
+        form = ContactForm()
+    else:
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            from_email = form.cleaned_data['from_email']
+            message = form.cleaned_data['message']
+            try:
+                send_mail(subject, message, from_email, [DEFAULT_FROM_EMAIL],)
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            return redirect('success')
+    return render(request, "email.html", {'form': form})
+
+def success(request):
+    return HttpResponse('Success! Thank you for your message.')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            mail_subject = 'Activate your account.'
+            message = render_to_string('activate.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+                'token':account_activation_token.make_token(user),
+            })
+            from_email = form.cleaned_data.get('email')
+            #email = EmailMessage(
+            #            mail_subject, message, to=[to_email]
+            #)
+            #email.send()
+            send_mail(mail_subject,message, from_email, [DEFAULT_FROM_EMAIL])
+            return HttpResponse('Please confirm your email address to complete the registration')
+    else:
+        form = SignupForm()
+    return render(request, 'signup.html', {'form': form})
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+        #return JsonResponse( {'email': user.email } )
+    else:
+        return HttpResponse('Activation link is invalid!')
+
+
+
+
+
